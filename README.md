8ab7db43a9e4271ce8ce2e4548c01f36fb7945d0
Claire Hsieh <chsieh018@gmail.com>
2023-07-18 21:44:15 -0700

    updated viterbi and README

--- README.md
+++ README.md
@@ -1,13 +1,45 @@
 # Viterbi Algorithm
 
-## SAMPLE INPUT <br>
-state = [[0.5, 0.5]] <br>
-transition = [[0.5,0.4], [0.4,0.6]] <br>
-transition_states = ['H', 'L'] <br>
-emission = [[0.2,0.3,0.3,0.2], [0.3,0.2,0.2,0.3]] # hidden states, row: A,C,G,T, col: H, L <br>
-emission_states = ['A', 'C', 'G', 'T'] <br>
-seq = 'GGCACTGAA' <br>
-viterbi(state, transition, transition_states, emission, emission_states, seq) <br>
+A simple viterbi decoding algorithm implementation in python.
 
-## SAMPLE OUTPUT <br>
-(['Healthy', 'Healthy', 'Fever'], -11.772868005544115)
+## Usage
+
+Transition, emission, and state probabilities are defined in a json file. The format of the file is as follows:
+
+```json
+{
+    "states": ["H", "C"],
+    "start_prob": {
+        "H": 0.6,
+        "C": 0.4
+    },
+    "transition_prob": {
+        "H": {
+            "H": 0.7,
+            "C": 0.3
+        },
+        "C": {
+            "H": 0.4,
+            "C": 0.6
+        }
+    },
+    "emission_prob": {
+        "H": {
+            "1": 0.1,
+            "2": 0.4,
+            "3": 0.5
+        },
+        "C": {
+            "1": 0.7,
+            "2": 0.2,
+            "3": 0.1
+        }
+    }
+}
+```
+
+To run the algorithm, simply run the following command:
+
+`viterbi(transition_file, sequence)`
+
+Where `transition_file` is the path to the json file containing the transition, emission, and state probabilities, and `sequence` is the sequence of observations.
--- viterbi.py
+++ viterbi.py
@@ -10,56 +10,44 @@ def format_prob(state, transition, transition_states, emission, emission_states)
     df_em = pd.DataFrame(emission, index = transition_states, columns = emission_states,)
     return df_state, df_trans, df_em
 
-def viterbi(state, transition, transition_states, emission, emission_states, seq, log=0):
-    # input: list of state, transition, and emission prob as lists
-    # state should be in format: [[p1, p2, ...]]
-    # transition indices: list of states
-    state, transition, emission = format_prob(state, transition, transition_states, emission, emission_states)
-    
-    if transition.shape[1]!= emission.shape[0]: 
-        # num rows of trans. states must equal num col. of emission states
-        print("Number of rows in transition state must equal number of columns in emission states", file=sys.stderr)
-    probability = 1
-    path = []
-    if type(seq) == str:
-        seq = [*seq]
-    for s,i in zip(seq, range(len(seq))):
-        if i == 0: # if first in seq, use state prob.
-            prob1 = {}
-            for st in state:
-                prob1[st] = float(state[st] * emission[s][st]) 
-            t_state = max(prob1, key=prob1.get)
-            path.append(t_state) # add max probability to path
-            if log == 1:
-                prev_prob = math.log2(prob1[path[0]])
-            else:
-                prev_prob = prob1[path[0]]
-            prev_state = t_state
-        else:
-            prob = {}           
-            for t in transition:      
+def viterbi(transition_file, seq, log=1):
+    state = {}
+    transition = {}
+    emission = {}
+    with open(transition_file) as f:
+        x = json.load(f)
+        for i in range(x['states']):
+            temp = x['state'][i]
+            state[temp['name']] = temp['init']
+            transition[temp['name']] = temp['transition']
+            emission[temp['name']] = temp['emission']
+        if len(transition) != len(emission):
+            raise ValueError("Transition and emission matrices must be the same length")
+        probability = 1
+        path = []
+        if type(seq) == str:
+            seq = [*seq]
+        for s,i in zip(seq, range(len(seq))):
+            if i == 0: # if first in seq, use state prob.
+                prob1 = {}
+                for st in state:
+                    prob1[st] = float(state[st] * emission[st][s]) 
+                t_state = max(prob1, key=prob1.get)
+                path.append(t_state) # add max probability to path
                 if log == 1:
-                    prob[t] = prev_prob +  math.log2(transition[prev_state][t] * emission[s][t])
+                    prev_prob = math.log2(prob1[path[0]])
                 else:
-                    prob[t] = prev_prob * (transition[prev_state][t] * emission[s][t])
-            x = max(prob, key=prob.get)
-            path.append(x)
-            prev_prob = prob[x]
-            prev_state = x
+                    prev_prob = prob1[path[0]]
+                prev_state = t_state
+            else:
+                prob = {}           
+                for t in transition:      
+                    if log == 1:
+                        prob[t] = prev_prob +  math.log2(transition[prev_state][t] * emission[t][s])
+                    else:
+                        prob[t] = prev_prob * (transition[prev_state][t] * emission[t][s])
+                x = max(prob, key=prob.get)
+                path.append(x)
+                prev_prob = prob[x]
+                prev_state = x
     return path, prev_prob
-
-
-
-"""
-SAMPLE INPUT
-
-state = [[0.5, 0.5]]
-transition = [[0.5,0.4], [0.4,0.6]] 
-transition_states = ['H', 'L']
-emission = [[0.2,0.3,0.3,0.2], [0.3,0.2,0.2,0.3]] # hidden states, row: A,C,G,T, col: H, L
-emission_states = ['A', 'C', 'G', 'T']
-seq = 'GGCACTGAA'
-
-viterbi(state, transition, transition_states, emission, emission_states, seq)
-
-"""
