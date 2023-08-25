be63386e27797de96e0140d51baad550f7ab57ed
Claire Hsieh <chsieh018@gmail.com>
2023-08-25 06:39:55 -0700

    finished multiple traceback :)

--- stochastic_viterbi/stochastic_viterbi.py
+++ stochastic_viterbi/stochastic_viterbi.py
@@ -1,8 +1,7 @@
 import math
 import argparse
-import sys
-import gzip
 import json
+import random
 
 # parser = argparse.ArgumentParser(
 # 	description='General Stochastic Viterbi Algorithm')
@@ -11,7 +10,7 @@ import json
 # parser.add_argument('jsonhmm', type=str, required=True, help="json file of transition and emission probabilities")
 # arg = parser.parse_args()
 
-def viterbi(transition_file, seq, log=1):
+def stochastic_viterbi(transition_file, seq, tr):
     state = {}
     transition = {}
     emission = {}
@@ -25,7 +24,7 @@ def viterbi(transition_file, seq, log=1):
         if len(transition) != len(emission):
             raise ValueError("Transition and emission matrices must be the same length")
         trellis = []
-        path = []
+        paths = []
         if type(seq) == str:
             seq = [*seq]
         # fill out viterbi trellis
@@ -45,21 +44,60 @@ def viterbi(transition_file, seq, log=1):
                     for trans2 in prob1[trans1].keys(): # iterate through current states
                         # print(trans1, trans2, max_prev, path[i-1][max_prev])#, math.log(transition[trans1][trans2])), math.log(emission[trans2][s]))
                         prob1[trans1][trans2] = max_prev + max_prev + math.log(transition[trans1][trans2]) + math.log(emission[trans2][s])
-            trellis.append(prob1)
+            trellis.append(prob1)       
 
         # traceback
-        temp = dict((key, {}) for key in state.keys())
-        for i in range(len(trellis)-1, 1, -1):
+        multiple_traceback(trellis, state, paths, tr)
+        # elif tr == 1: # multiple traceback
+            # loop through all 
+            # for i in range(len(trellis)):
+                
+
+        return trellis, paths
+
+def traceback(trellis, state, path):
+    temp = dict((key, {}) for key in state.keys())
+    for i in range(len(trellis)-1, 1, -1):
+        for trans1 in trellis[i]: # loop through last state
+            for trans4 in trellis[i-1]: # loop through second to last state
+                max_state = max(trellis[i-1][trans4], key= lambda x: trellis[i-1][trans4][x])    
+                temp[trans4][max_state] = trellis[-1][trans4][max_state]
+        max_state = max(temp, key= lambda x: list(temp[x].values())[0])
+        path.append(list(temp[max_state].keys())[0]) # add last state to path
+        path.append(max_state) # add second to last state to path      
+    return path
+
+def multiple_traceback(trellis, state, paths, tr):
+    # tr = number of tracebacks
+    temp = dict((key, {}) for key in state.keys())
+    path = []
+    for p in range(tr):
+        max_state = random.choice(list(state.keys()))
+        for i in range(len(trellis)-2, 1, -1):
             for trans1 in trellis[i]: # loop through last state
                 for trans4 in trellis[i-1]: # loop through second to last state
                     max_state = max(trellis[i-1][trans4], key= lambda x: trellis[i-1][trans4][x])    
                     temp[trans4][max_state] = trellis[-1][trans4][max_state]
-            max_state = max(temp, key= lambda x: list(temp[x].values())[0])
-            path.append(list(temp[max_state].keys())[0]) # add last state to path
-            path.append(max_state) # add second to last state to path      
-    return path
+        max_state = max(temp, key= lambda x: list(temp[x].values())[0])
+        path.append(list(temp[max_state].keys())[0]) # add last state to path
+        path.append(max_state) # add second to last state to path 
+        paths.append(path) 
+    return paths
 
 def forward_backward(json_file, seq, log=1):
+    state = {}
+    transition = {}
+    emission = {}
+    with open(json_file) as f:
+        x = json.load(f)
+        for i in range(x['states']):
+            temp = x['state'][i]
+            state[temp['name']] = temp['init']
+            transition[temp['name']] = temp['transition']
+            emission[temp['name']] = temp['emission']
+        if len(transition) != len(emission):
+            raise ValueError("Transition and emission matrices must be the same length")
+        
     # Compute forward probability
     forward = list(dict((key, 0) for key in transition.keys()) for i in range(len(seq)))
     for ind, o in enumerate(seq):
@@ -106,8 +144,14 @@ def forward_backward(json_file, seq, log=1):
         for t in smoothed[ind]:
             smoothed[ind][t] /= tot
     
-    # Traceback (smoothed values)
-
-    
     return forward, backward, smoothed
 
+
+### FORMAT OF TRELLIS ###
+# trellis = [{initial_state: log_probability}, {current_state: {previous_state: log_probability}, ... }]
+
+### TO DO ###
+# calculate probability of path 
+# apply forward backward to path (seperate)
+# do MULTIPLE traceback: add random element 
+# figure out way to do documentation
